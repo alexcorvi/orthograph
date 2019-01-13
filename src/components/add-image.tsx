b7aa911d88624679f2a2ad945c5d9aba26937867
Alex Corvi <ali.a.saleem@outlook.com>
2019-01-13 16:55:06 +0300

    choosing and dismissing the same image should work

--- src/components/add-image.tsx
+++ src/components/add-image.tsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { AddAPhoto } from '@material-ui/icons';
-import { Button, Paper, Typography } from '@material-ui/core';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { AddAPhoto } from "@material-ui/icons";
+import { Button, Paper, Typography } from "@material-ui/core";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
 @observer
-export class AddImage extends React.Component<{ title: string; onSelect: (src: string) => void }> {
+export class AddImage extends React.Component<{
+	title: string;
+	onSelect: (src: string) => void;
+}> {
 	@observable id: number = Math.round(Math.random() * 1000);
 
 	render() {
 		return (
 			<div className="add-image">
-				<Typography variant="overline">Add {this.props.title}</Typography>
-				<label htmlFor={'upload' + this.id}>
+				<Typography variant="overline">
+					Add {this.props.title}
+				</Typography>
+				<label htmlFor={"upload" + this.id}>
 					<Button
 						color="default"
 						variant="outlined"
 						component="span"
-						style={{ border: '1px dashed #e3e3e3' }}
+						style={{ border: "1px dashed #e3e3e3" }}
 					>
 						<AddAPhoto fontSize="large" />
 					</Button>
@@ -26,19 +31,20 @@ export class AddImage extends React.Component<{ title: string; onSelect: (src: s
 				<input
 					hidden
 					accept="image/*"
-					id={'upload' + this.id}
+					id={"upload" + this.id}
 					multiple={false}
 					type="file"
-					onChange={(e) => {
+					onChange={e => {
 						if (e.target.files && e.target.files.length > 0) {
 							const reader = new FileReader();
-							reader.addEventListener('load', () => {
-								if (typeof reader.result === 'string') {
+							reader.addEventListener("load", () => {
+								if (typeof reader.result === "string") {
 									this.props.onSelect(reader.result);
 								}
 							});
 							reader.readAsDataURL(e.target.files[0]);
 						}
+						e.target.value = "";
 					}}
 				/>
 			</div>
